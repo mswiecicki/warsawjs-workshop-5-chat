41887dc696056d835ba75835b0cbf68c3be370b8
Marek Święcicki <marekswiecicki@protonmail.com>
2017-04-01 21:29:31 +0200

    add clearing prompt

--- client.js
+++ client.js
@@ -3,7 +3,13 @@
 const io = require('socket.io-client')
 const socket = io('http://localhost:3000'); // now the client connects to the server
 
+function clearPrompt() {
+    process.stdout.cursorTo(0);
+    process.stdout.clearLine();
+}
+
 socket.on('message', (msg) => {
+    clearPrompt();
     console.log(`==> \"${msg}\"`);
     readline.prompt();
 });
